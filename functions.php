499fe279e5feeffeae6041095ccff875964bc0d3
Monirul Islam <mdmonirulislammonir@gmail.com>
2024-03-14 16:42:13 +0600

    jquery

--- functions.php
+++ functions.php
@@ -23,7 +23,12 @@ function vclbd_scripts() {
  * Add styles
  */
 	wp_enqueue_style( 'bootstrap-core', get_template_directory_uri() .'/css/bootstrap.min.css');
+    wp_enqueue_style( 'fontawesome', get_template_directory_uri() .'/css/fontawesome/css/all.min.css');
+    wp_enqueue_style( 'custom', get_template_directory_uri() .'/style.css');
+
+    wp_enqueue_script('bootstrap-js', get_template_directory_uri().'/js/bootstrap.min.js', array('jquery'), null, true);
+
 }
 // Register style sheet.
-add_action( 'wp_enqueue_scripts', 'vclbd_scripts' );
+add_action( 'wp_enqueue_scripts', 'vclbd_scripts');
 ?>
