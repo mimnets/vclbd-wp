90a1998c52ebc0bc7b38691c115cc04637d0c1a8
Monirul Islam <mdmonirulislammonir@gmail.com>
2024-03-14 10:43:18 +0600

    added if statement to check content width

--- functions.php
+++ functions.php
@@ -5,7 +5,16 @@
 ?>
 
 <?php
+
+if(! isset($content_width){
+    $content_width = 660;
+})
+
 function vclbdwp_setup(){
     add_theme_support('automatic-feed-links');
     add_theme_support('title-tag');
 }
+
+add_action('after_setup_theme','vclbdwp_setup')
+
+?>
