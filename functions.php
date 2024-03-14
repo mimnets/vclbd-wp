38c029a553da50ab1ecaec69d4e2fe98e6d57383
Monirul Islam <mdmonirulislammonir@gmail.com>
2024-03-14 10:38:59 +0600

    adding php tags

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+/*
+* Functions definitions for VCLBD WordPress theme
+*/
+?>
+
+<?php
+function vclbdwp_setup(){
+    add_theme_support('automatic-feed-links');
+    add_theme_support('title-tag');
+}
---
+++ header.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>VCLBD | Versatile Creation Ltd.</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    
+    <!-- Custom Fonts-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;1,300&display=swap" rel="stylesheet">
+    
+    <!-- Font Awesome CSS -->
+    <link rel="stylesheet" href="css/fontawesome/css/all.css">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/custom.css">
+  </head>
+  <body>
+    <header>
+        <nav class="navbar navbar-expand-md navbar-light bg-light">
+            <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">VCLBD</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.html">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="blog.html">Blog</a>
+                    </li>
+                </ul>
+                </div>
+            </div> 
+        </nav>
+        <section class="container-fluid">
+            <div class="row position-relative d-flex justify-content-center align-items-center text-center">
+            <img src="images/lake.jpg" alt="lake background" class="header_img">
+            <div class="position-absolute feature-bg">
+                <p class="feature-text">FULL RESPONSIVE<br> PREMIUM CORPORATE THEME<br> FOR WORDPRESS.</p>
+            </div>    
+            </div>
+        </section>
+    </header>
