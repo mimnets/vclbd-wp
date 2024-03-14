36692f5e4fe11249d8980bba98b3b902169cdd42
Monirul Islam <mdmonirulislammonir@gmail.com>
2024-03-14 15:54:20 +0600

    register the bootstrap.min.css to show the styles

--- functions.php
+++ functions.php
@@ -10,11 +10,20 @@ if(! isset($content_width)){
     $content_width = 660;
 }
 
-function vclbdwp_setup(){
+function vclbd_setup(){
     add_theme_support('automatic-feed-links');
     add_theme_support('title-tag');
 }
 
-add_action('after_setup_theme','vclbdwp_setup')
+add_action('after_setup_theme','vclbd_setup');
 
+
+function vclbd_scripts() {
+ /*
+ * Add styles
+ */
+	wp_enqueue_style( 'bootstrap-core', get_template_directory_uri() .'/css/bootstrap.min.css');
+}
+// Register style sheet.
+add_action( 'wp_enqueue_scripts', 'vclbd_scripts' );
 ?>
